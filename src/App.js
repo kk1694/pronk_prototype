a4c3c7792b05fd9667d0f9ad043102f87c23b6cc
krisztian <krisztiankovacs@fastmail.com>
2021-08-14 20:16:49 +0100

    Add auth-

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 import React from "react";
 import { ThemeProvider } from '@material-ui/core/styles';
 import { createTheme } from '@material-ui/core/styles';
+import { useAuth0 } from '@auth0/auth0-react';
+
 
 import TappyWindow from './components/TappyWindow';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
 
 const theme = createTheme({
 
@@ -11,8 +15,15 @@ const theme = createTheme({
 
 function App() {
 
+  const { isLoading, isAuthenticated} = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>
+
+  if (!isAuthenticated) return <LoginButton/>
+  
   return (
     <ThemeProvider theme={theme}>
+    <LogoutButton></LogoutButton>
     <div className="App">
       <TappyWindow/>
     </div>
